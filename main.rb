e3b7d519fb8f87e096a3a963d36ecc9e29f3f1b7
blpeters <brett.peters@utexas.edu>
2022-11-26 16:57:59 -0600

    basic pseudo instructions

---
+++ main.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# knights travails
+# 
+# pseudocode:
+# GOAL: output the shortest path for a knight to get from one square to another, listing the squares it takes along the way.
+# put together a script that creates a game board and a knight
+# Treat all possible moves the knight could make as children in a tree.
+# Don't allow any moves to go off the board.
+# Decide which search algorithm is best to use for this case. One of them could be a potentially infinite series.
+# Use the chosen search algorithm to find the shortest path between the starting square (or node) and the ending square. Output the full path.
