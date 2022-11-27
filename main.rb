9ccbbf2ca3025fe1c8e99c5eec85fd6b607a547a
blpeters <brett.peters@utexas.edu>
2022-11-27 17:49:44 -0600

    improved BFS pseudocode

--- main.rb
+++ main.rb
@@ -4,14 +4,27 @@
 
 # pseudocode:
 # GOAL: output the shortest path for a knight to get from one square to another, listing the squares it takes along the way.
-# put together a script that creates a game board and a knight
-# Treat all possible moves the knight could make as children in a tree.
-# Don't allow any moves to go off the board.
-# Decide which search algorithm is best to use for this case. One of them could be a potentially infinite series.
-# edge lists, adjacency matrices, adjacency lists. Can also be directed or undirected.
-# breadth first search will find the shortest distance between two squares - option?
+# put together a script that creates a game board and a knight (not sure I need a Knight class....)
 
-# BFS:
+# board class
+# initialize with an array containing 64 nodes (or vertices), each represented as a subarray containing a coordinate subarray, a distance variable, and a predecessor_node variable
+# when knight_moves(start, end) is called, traverse graph using start coordinates:
+
+# until the queue is clear:
+# put node with start coordinates into a queue array with distance = 0, predecessor stays as nil
+
+# REPEAT THESE STEPS
+# shift node out of queue array (dequeue)
+
+# for each possible move (node) from the previous node: (make separate array with all possible next coordinates)
+# if the node has not yet been visited, (find each possible move coordinates in graph array and check if the distance variable is nil or not)
+# enqueue node and update the distance + 1 and predecessor_node variable
+
+# return to repeat (dequeue)
+
+# Don't allow any moves to go off the board.
+
+# CHOSEN ALGORITHM - graph with BFS:
 # set the distance and predecessor of each vertex to nil.
 # start the search at the source(the root node or the knight's position)
 # give each neighbor a distance of 1 and set it's predecessor to be the source.
@@ -22,5 +35,4 @@
 # how to tell if a node has been visited yet - only if the distance value is still nil, which was initialized at the start.
 # QUEUE: when we first visit a vertex, enqueue it.
 
-# look into dijkstra's algorithm - try solving first though.
-# Use the chosen search algorithm to find the shortest path between the starting square (or node) and the ending square. Output the full path.
+# Output the full path that the knight ends up taking to get to the ending square.
