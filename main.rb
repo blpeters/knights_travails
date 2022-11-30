3f9b93f7d5f6ef7106f638e6707a1ee410177a8c
blpeters <brett.peters@utexas.edu>
2022-11-29 19:58:17 -0600

    working solution

--- main.rb
+++ main.rb
@@ -1,118 +1,138 @@
 # frozen_string_literal: true
 
-# knights travails
+# knights travails pseudocode:
+# initialize with an array containing 64 nodes, each represented as a subarray containing an
+# x-coordinate, y-coordinate, a distance variable, and a predecessor_node variable.
+# put node with starting coordinates into a queue array with distance = 0, predecessor stays as nil
 
-# pseudocode:
-# GOAL: output the shortest path for a knight to get from one square to another, listing the squares it takes along the way.
-# put together a script that creates a game board and a knight (not sure I need a Knight class....)
+# REPEAT THESE STEPS until the queue is clear: (BFS GRAPH TRAVERSAL)
+# shift node out of queue array (dequeue)
+# for each possible move (node) from the previous node: make separate array of possible moves with all possible next squares
+# for each possible move, if the node has not yet been visited:
+#   enqueue node and update the distance + 1 and set predecessor_node variable to point to the current node.
 
-# board class
-# initialize with an array containing 64 nodes (or vertices), each represented as a subarray containing a coordinate subarray, a distance variable, and a predecessor_node variable
+# RETURN TO TOP
+
+# Once board has been traversed, find the square containing the end coordinates, and print out each square
+# by printing the node, following the predecessor node variable to find the next node, print, etc. until
+# reaching the starting square where the predecessor variable == nil.
 class Board
-  attr_accessor :queue, :board, :position_index
+  attr_accessor :queue, :board, :position_index, :distance, :starting_square
 
   def initialize
     @board = create_board
     @queue = []
     @position_index = nil
+    @distance = 0
+    @starting_square = nil
   end
 
   def knight_moves(starting_coordinates, ending_coordinates)
-    # when knight_moves(start, end) is called, traverse graph using start coordinates:
-    set_starting_square(starting_coordinates)
-    enqueue(position_index)
-    # until queue.empty?
-
-    dequeue
-    find_possible_moves
-    p board
-    # enqueue
-    
-    # end
-
+    reset_variables(starting_coordinates)
+    enqueue([position_index])
+    until queue.empty?
+      dequeue()
+      moves = find_possible_moves()
+      enqueue(find_move_indices(moves))
+    end
+    display_path(ending_coordinates)
   end
 
   private
 
   def create_board
     board_arr = []
-    (1..7).each do |i|
-      (1..7).each do |j|
+    (1..8).each do |i|
+      (1..8).each do |j|
+        # format is x coord, y coord, distance (levels of BFS) from start, predecessor node
         board_arr << [i, j, nil, nil]
       end
     end
     board_arr
   end
 
-  def set_starting_square(coordinates)
+  def reset_variables(coordinates)
+    @board = create_board
+    @position_index = nil
+    @distance = 0
+    @starting_square = coordinates
     board.each_with_index do |square, index|
       if square[0..1] == coordinates
-        square[2] = 0
+        square[2] = distance
         return @position_index = index
       end
-    end  
+    end
   end
 
-  def enqueue(position_index)
-    queue << position_index
+  def enqueue(index_arr)
+    index_arr.each do |index|
+      board[index][2] = board[position_index][2] + 1
+      board[index][3] = position_index unless board[index][0..1] == @starting_square
+      queue << index
+    end
   end
 
   def dequeue
-    position_index = queue.shift
+    @position_index = @queue.shift
   end
 
   def find_possible_moves
-    possible_moves_arr = []
-    a, b = board[position_index][0..1] # 2, 1
+    moves = []
+    a, b = board[position_index][0..1]
     move_distances = [[-2, -1], [-2, 1], [-1,-2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]]
     move_distances.each do |move|
       c, d = move
       x = a + c
       y = b + d
-      possible_moves_arr << [x, y] if valid_square?(x, y) && !square_visited?(x, y)
-    end  
-    p possible_moves_arr
+      moves << [x, y] if valid_square?(x, y) && !square_visited?(x, y)
+    end
+    moves
   end
 
-  def valid_square?(x, y)
-    return false if !(1..7).include?(x) || !(1..7).include?(y)
+  def valid_square?(x_coord, y_coord)
+    return false if !(1..8).include?(x_coord) || !(1..8).include?(y_coord)
+
     true
   end
 
   def square_visited?(x, y)
-    board.each do |square|
-      return true if square[0..1] == [x, y] && square[2] != nil
-    end
+    board.each { |square| return true if square[0..1] == [x, y] && !square[2].nil? }
     false
   end
 
-# until the queue is clear:
-# put node with start coordinates into a queue array with distance = 0, predecessor stays as nil
+  def find_move_indices(moves)
+    indices = []
+    moves.each do |move|
+      board.each_with_index do |square, index|
+        indices << index if square[0..1] == move
+      end
+    end
+    indices
+  end
 
-# REPEAT THESE STEPS
-# shift node out of queue array (dequeue)
-
-# for each possible move (node) from the previous node: (make separate array with all possible next coordinates)
-# if the node has not yet been visited, (find each possible move coordinates in graph array and check if the distance variable is nil or not)
-# enqueue node and update the distance + 1 and predecessor_node variable
-
-# return to repeat (dequeue)
-
-# Don't allow any moves to go off the board.
-
-# CHOSEN ALGORITHM - graph with BFS:
-# set the distance and predecessor of each vertex to nil.
-# start the search at the source(the root node or the knight's position)
-# give each neighbor a distance of 1 and set it's predecessor to be the source.
-# Then, visit all the neighbors of the vertices whose distance is 1 AND that have not been visited before, and give each of these vertices a...
-# distance of 2 and set its predecessor to be vertex from which we visited it.
-# keep going untiall all vertices reachable from teh source vertex have been visited, always visiting all vertices at distance k from ...
-# the source before visiting any vertex at distance k + 1
-# how to tell if a node has been visited yet - only if the distance value is still nil, which was initialized at the start.
-# QUEUE: when we first visit a vertex, enqueue it.
-
-# Output the full path that the knight ends up taking to get to the ending square.
+  def display_path(ending_coordinates)
+    path = []
+    current_square = nil
+    board.each do |square| 
+      current_square = square if square[0..1] == ending_coordinates
+    end
+    while true
+      path << current_square[0..1]
+      index = current_square[3]
+      break if index.nil?
+      current_square = board[index]
+    end
+    puts "Path found! It will take the knight #{path.length} moves! Here's the path:"
+    path.each do |square| 
+      print '   '
+      p square
+    end
+    puts
+  end
 end
 
 board = Board.new
-board.knight_moves([2,1], [3,4])
+board.knight_moves([2, 1], [3, 4])
+board.knight_moves([3, 7], [7, 7])
+board.knight_moves([6, 6], [1, 8])
+board.knight_moves([1, 1], [1, 8])
